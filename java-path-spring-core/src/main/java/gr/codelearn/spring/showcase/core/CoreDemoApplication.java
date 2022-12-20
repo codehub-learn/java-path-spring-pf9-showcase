4f7f627837934ad166926c5a6a43b5f071dd3161
Christos Peristeris <chrisperis@gmail.com>
2022-12-20 22:44:40 +0200

    Resolve dependency issue, with CoreDemoApplication pretending to be the one providing the dependencies (acting as a wannabe Spring container).
    However, this indicates that we want to delegate dependencies and define them before runtime.

--- java-path-spring-core/src/main/java/gr/codelearn/spring/showcase/core/CoreDemoApplication.java
+++ java-path-spring-core/src/main/java/gr/codelearn/spring/showcase/core/CoreDemoApplication.java
@@ -2,10 +2,11 @@ package gr.codelearn.spring.showcase.core;
 
 import gr.codelearn.spring.showcase.core.domain.Bike;
 import gr.codelearn.spring.showcase.core.domain.Car;
+import gr.codelearn.spring.showcase.core.domain.Engine;
+import gr.codelearn.spring.showcase.core.domain.EngineType;
 import gr.codelearn.spring.showcase.core.service.TravelService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 
 public class CoreDemoApplication {
 	//remember to perform a clean after adding logging configuration, before executing class
@@ -14,10 +15,19 @@ public class CoreDemoApplication {
 	public static void main(String[] args) {
 		logger.info("Starting program execution");
 
-		TravelService myTravel = new TravelService(new Car());
+		/*	Attempting to start a travel with a vehicle without an engine, will provide a null pointer exception.
+			This main method, pretending to be the container providing the dependencies, is delegated to provide
+			all needed dependencies for a travel to be performed
+		*/
+
+		//TravelService myTravel = new TravelService(new Car());
+		TravelService myTravel = new TravelService(
+				Car.builder().engine(Engine.builder().engineType(EngineType.DIESEL).build()).build());
 		myTravel.startJourney();
 
-		TravelService anotherTravel = new TravelService(new Bike());
+		//TravelService anotherTravel = new TravelService(new Bike());
+		TravelService anotherTravel = new TravelService(
+				Bike.builder().engine(Engine.builder().engineType(EngineType.ELECTRIC).build()).build());
 		anotherTravel.startJourney();
 	}
 
