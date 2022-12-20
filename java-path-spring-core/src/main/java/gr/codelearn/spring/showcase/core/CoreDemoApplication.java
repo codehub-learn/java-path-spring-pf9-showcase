4b80fb77d22b4e472f7b737df3b4ce5a18d6304d
Christos Peristeris <chrisperis@gmail.com>
2022-12-20 22:09:06 +0200

    Add example of highly coupled classes, highlight need for inversion of control

---
+++ java-path-spring-core/src/main/java/gr/codelearn/spring/showcase/core/CoreDemoApplication.java
@@ -0,0 +1,21 @@
+package gr.codelearn.spring.showcase.core;
+
+import gr.codelearn.spring.showcase.core.service.TravelService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class CoreDemoApplication {
+	//remember to perform a clean after adding logging configuration, before executing class
+	private static final Logger logger = LoggerFactory.getLogger(CoreDemoApplication.class);
+
+	public static void main(String[] args) {
+
+		logger.info("Starting program execution");
+		TravelService myTravel = new TravelService();
+		myTravel.startJourney();
+	}
+
+}
