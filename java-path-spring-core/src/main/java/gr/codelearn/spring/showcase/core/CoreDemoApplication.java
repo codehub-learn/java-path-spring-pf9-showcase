ac5f813af64c8a6b85646056e654909181e883bd
Christos Peristeris <chrisperis@gmail.com>
2022-12-20 23:01:27 +0200

    Add spring bean definition example, using Java Configuration, @Configuration annotated class

--- java-path-spring-core/src/main/java/gr/codelearn/spring/showcase/core/CoreDemoApplication.java
+++ java-path-spring-core/src/main/java/gr/codelearn/spring/showcase/core/CoreDemoApplication.java
@@ -6,9 +6,11 @@ import gr.codelearn.spring.showcase.core.domain.Vehicle;
 import gr.codelearn.spring.showcase.core.service.TravelService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+@SpringBootApplication
 public class CoreDemoApplication {
 	//remember to perform a clean after adding logging configuration, before executing class
 	private static final Logger logger = LoggerFactory.getLogger(CoreDemoApplication.class);
@@ -16,15 +18,29 @@ public class CoreDemoApplication {
 	public static void main(String[] args) {
 		logger.info("Starting program execution");
 
-		ApplicationContext context = new AnnotationConfigApplicationContext("gr.codelearn.spring.showcase.core");
+		ApplicationContext context = SpringApplication.run(CoreDemoApplication.class, args);
 
+		/*
+			this bean is still being picked up, although not present in VehicleConfig, but annotated with @Component,
+			due to @SpringBootApplication component scanning
+		*/
 		Vehicle vehicle = context.getBean("car", Car.class);
+		TravelService anotherTravel = new TravelService(vehicle);
+		anotherTravel.startJourney();
+
+		/*
+			Scanned in VehicleConfig
+		 */
+		vehicle = context.getBean("beanConfiguredCar", Car.class);
 		TravelService myTravel = new TravelService(vehicle);
 		myTravel.startJourney();
 
+		/*
+			Scanned in VehicleConfig, no @Component in Bike class
+		 */
 		vehicle = context.getBean("bike", Bike.class);
-		TravelService anotherTravel = new TravelService(vehicle);
-		anotherTravel.startJourney();
+		TravelService anotherTravel2 = new TravelService(vehicle);
+		anotherTravel2.startJourney();
 	}
 
 }
